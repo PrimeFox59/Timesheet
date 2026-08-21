57e91ef6fd7ca24b7b85d3a0d5f06f93d75f8437
primefox59 <primetroyxs@gmail.com>
2026-08-21 15:57:17 +0700

    Add serverExternalPackages better-sqlite3 to next.config.ts

--- nextjs-app/next.config.ts
+++ nextjs-app/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/timesweet',
+  serverExternalPackages: ['better-sqlite3'],
 };
+
 
 
 export default nextConfig;
