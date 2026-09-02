7f8945f2dea7448efb9d3da93ce2fa964c7531e8
primefox59 <primetroyxs@gmail.com>
2026-09-02 09:41:18 +0700

    fix(pm2): dynamically configure cwd and port based on OS platform

--- nextjs-app/ecosystem.config.js
+++ nextjs-app/ecosystem.config.js
@@ -1,13 +1,15 @@
+const isWindows = process.platform === 'win32';
+
 module.exports = {
   apps: [
     {
       name: 'timesheet-metso',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 3005',
-      cwd: '/home/Prime-Projectx/timesheet/nextjs-app',
+      args: isWindows ? 'start -p 8565' : 'start -p 3005',
+      cwd: isWindows ? 'D:/0 Running apps/Timesheet/nextjs-app' : '/home/Prime-Projectx/timesheet/nextjs-app',
       env: {
         NODE_ENV: 'production',
-        PORT: 3005
+        PORT: isWindows ? 8565 : 3005
       }
     }
   ]
